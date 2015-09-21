131d969e0063a552f8c297000ab4f5c2318bd728
joycsc <joy_csharp@yahoo.com>
2015-09-21 16:37:11 +0800

    - LoginViewModel.cs added

---
+++ Migration/Eisk.Web/Models/View Models/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿/****************** Copyright Notice *****************
+ 
+This code is licensed under Microsoft Public License (Ms-PL). 
+You are free to use, modify and distribute any portion of this code. 
+The only requirement to do that, you need to keep the developer name, as provided below to recognize and encourage original work:
+
+=======================================================
+   
+Architecture Designed and Implemented By:
+Mohammad Ashraful Alam
+Microsoft Most Valuable Professional, ASP.NET 2007 – 2013
+Twitter: http://twitter.com/AshrafulAlam | Blog: weblogs.asp.net/ashraful | Github: https://github.com/ashrafalam
+   
+*******************************************************/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Eisk.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+}
