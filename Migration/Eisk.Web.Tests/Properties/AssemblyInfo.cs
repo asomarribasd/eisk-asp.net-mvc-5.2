16af29545ca8b49db232d2fc9b47ca614a1de7bf
joycsc <joy_csharp@yahoo.com>
2015-09-21 20:00:28 +0800

    xUnit config updated.

--- Migration/Eisk.Web.Tests/Properties/AssemblyInfo.cs
+++ Migration/Eisk.Web.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Xunit;
 
@@ -15,7 +14,7 @@ using Xunit;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = true)]
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
