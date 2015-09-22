c59c89aff6a7a15ef4fd44255e69ca5713fe2eae
joycsc <joy_csharp@yahoo.com>
2015-09-22 16:11:26 +0800

    refactor.

---
+++ Migration/Eisk.Web/Data Access/UserDataAccess.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using Eisk.Models;
+
+namespace Eisk.DataAccess
+{
+    public static class UserDataAccess
+    {
+        public static IEnumerable<UserInfo> GetAll()
+        {
+            return UserInMemoryDataSource.ToList();
+        }
+
+        public static UserInfo GetByUserName(string userName)
+        {
+            return UserInMemoryDataSource.FirstOrDefault(u => u.UserName == userName);
+        }
+
+        public static UserInfo Validate(string userName, string password)
+        {
+            return UserInMemoryDataSource.FirstOrDefault(u => u.UserName == userName && u.Password == password);
+        }
+
+        static List<UserInfo> _userInMemoryDataSource;
+        static List<UserInfo> UserInMemoryDataSource
+        {
+            get
+            {
+                if (_userInMemoryDataSource == null)
+                {
+                    _userInMemoryDataSource = new List<UserInfo>();
+
+                    //add members
+                    for (int i = 0; i < 5; i++)
+                        _userInMemoryDataSource.Add(
+                            new UserInfo
+                            {
+                                UserName = "member" + (i + 1) + "@member.com",
+                                Password = "any"
+                            });
+
+                    //add administrators
+                    for (int i = 0; i < 5; i++)
+                        _userInMemoryDataSource.Add(
+                            new UserInfo
+                            {
+                                UserRole = UserRole.Administrator.ToString(),
+                                UserName = "admin" + (i + 1) + "@admin.com",
+                                Password = "any"
+                            });
+
+                }
+
+                return _userInMemoryDataSource;
+            }
+        }
+    }
+}
