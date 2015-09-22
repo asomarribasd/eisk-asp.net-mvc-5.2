4751793a9fd9ccc56d24401a62e419a30393e759
joycsc <joy_csharp@yahoo.com>
2015-09-22 19:35:59 +0800

    dbContext updated.

--- Migration/Eisk.Web/Controllers/EmployeesController.cs
+++ Migration/Eisk.Web/Controllers/EmployeesController.cs
@@ -1,20 +1,9 @@
-﻿using Eisk.DataAccess;
+﻿/************************************************************************************************************************************************
+*   Designed and Implemented By: Ashraf Alam, Microsoft MVP | Blog: http://weblogs.asp.net/ashraful | Github: https://github.com/ashrafalam     *                
+*************************************************************************************************************************************************/
+using Eisk.DataAccess;
 using Eisk.Helpers;
 using Eisk.Models;
-/****************** Copyright Notice *****************
- 
-This code is licensed under Microsoft Public License (Ms-PL). 
-You are free to use, modify and distribute any portion of this code. 
-The only requirement to do that, you need to keep the developer name, as provided below to recognize and encourage original work:
-
-=======================================================
-   
-Architecture Designed and Implemented By:
-Mohammad Ashraful Alam
-Microsoft Most Valuable Professional, ASP.NET 2007 – 2013
-Twitter: http://twitter.com/AshrafulAlam | Blog: http://weblogs.asp.net/ashraful | Github: https://github.com/ashrafalam
-   
-*******************************************************/
 using System;
 using System.Data.Entity;
 using System.Drawing;
--- Migration/Eisk.Web/Data Access/DatabaseContext.cs
+++ Migration/Eisk.Web/Data Access/DatabaseContext.cs
@@ -1,24 +1,22 @@
-﻿/****************** Copyright Notice *****************
- 
-This code is licensed under Microsoft Public License (Ms-PL). 
-You are free to use, modify and distribute any portion of this code. 
-The only requirement to do that, you need to keep the developer name, as provided below to recognize and encourage original work:
-
-=======================================================
-   
-Architecture Designed and Implemented By:
-Mohammad Ashraful Alam
-Microsoft Most Valuable Professional, ASP.NET 2007 – 2013
-Twitter: http://twitter.com/AshrafulAlam | Blog: http://weblogs.asp.net/ashraful | Github: https://github.com/ashrafalam
-   
-*******************************************************/
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using Eisk.Models;
 
 namespace Eisk.DataAccess
 {
-    public partial class DatabaseContext : DbContext
+    public class DatabaseContext : DbContext
     {
         public virtual IDbSet<Employee> EmployeeRepository { get; set; }
+
+        public DatabaseContext()
+        {
+            // Required to prevent bug - http://stackoverflow.com/questions/5737733
+            Configuration.ValidateOnSaveEnabled = false;
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        public new virtual IDbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
     }
 }
